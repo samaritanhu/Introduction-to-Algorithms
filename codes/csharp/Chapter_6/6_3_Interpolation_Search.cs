8efc6c84da3864af997cdc4cb36d405c19491034
Qucheng Miao <qcmiao1998@users.noreply.github.com>
2019-04-14 20:53:44 +0800

    Finish csharp 6.3 (#19)

---
+++ codes/csharp/Chapter_6/6_3_Interpolation_Search.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Introduction_to_Algorithms.Chapter_6
+{
+    public class Example_6_3
+    {
+        /// <summary>
+        /// Example 6.3: Interpolation Search
+        /// </summary>
+        /// <param name="x">A sorted array in the range 1 to n.</param>
+        /// <param name="z">The search key.</param>
+        /// <returns>Position (0 if no such index exist).</returns>
+        public static int Interpolation_Search(int[] x, int z)
+        {
+            if (x[0] > z || x[x.Length - 1] < z)
+                return 0;
+
+            int IntFind(int left, int right)
+            {
+                if (x[left - 1] == z)
+                    return left;
+                if (left == right || x[left - 1] == x[right - 1])
+                    return 0;
+                int nextGuess = left + (int)Math.Round((z - x[left - 1]) * (right - left) / (double)(x[right - 1] - x[left - 1]));
+                if (z < x[nextGuess - 1])
+                    return IntFind(left, nextGuess - 1);
+                return IntFind(nextGuess, right);
+            }
+
+            return IntFind(1, x.Length);
+        }
+    }
+}
