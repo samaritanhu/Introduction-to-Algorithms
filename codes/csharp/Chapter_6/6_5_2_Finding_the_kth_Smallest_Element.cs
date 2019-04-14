70e0d7783765a3e96d47f1d94f4a481b0316fde9
qcmiao1998 <qcmiao@hotmail.com>
2019-04-14 23:11:34 +0800

    Finish csharp 6.5.2

---
+++ codes/csharp/Chapter_6/6_5_2_Finding_the_kth_Smallest_Element.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Introduction_to_Algorithms.Chapter_6
+{
+    public class Example_6_5_2
+    {
+        public static int Selection(int[] x, int k)
+        {
+            if (k < 1 || k > x.Length)
+                throw new IndexOutOfRangeException();
+
+            int Partition(int left, int right)
+            {
+                int pivot = x[left - 1];
+                while (left < right)
+                {
+                    while (left < right && x[--right - 1] >= pivot)
+                        x[left - 1] = x[right - 1];
+                    while (left < right && x[++left - 1] <= pivot)
+                        x[right - 1] = pivot;
+                }
+
+                x[left - 1] = pivot;
+
+                return left;
+            }
+
+            int Select(int left, int right, int k2)
+            {
+                if (left == right)
+                    return left;
+                int middle = Partition(left, right);
+                if (middle - left + 1 >= k2)
+                    return Select(left, middle, k2);
+                return Select(middle + 1, right, k2 - (middle - left + 1));
+            }
+
+            int posi = Select(1, x.Length, k);
+            return x[posi - 1];
+        }
+    }
+}
