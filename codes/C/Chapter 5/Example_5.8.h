8d2b916e73210db7545ca74141361540c5453207
SmallY <45689960+iamSmallY@users.noreply.github.com>
2019-04-28 11:51:54 -0500

    Add Example 5.8 (#25)

---
+++ codes/C/Chapter 5/Example_5.8.h
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_58
+
+    #define EXAMPLE_58
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+// X is an array of some numbers and its  type is double.
+// n is the length of X.
+// It returns the the sum of X's maximum consecutive subsequence.
+
+double MaximumConsecutiveSubsequence(double* X , int n);
+
+#endif // EXAMPLE_58
